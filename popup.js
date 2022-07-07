71da3f8c604aba214d39f029dfd43434e49ae928
Tony <tonynguyenpersonal@gmail.com>
2022-07-07 04:45:16 -0700

    Changed popup when clicking on extension icon

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("button1").addEventListener("click", function(){
-        alert("The button has been clicked on.");
+        // alert("The button has been clicked on.");
         // var body = document.getElementsByTagName("body")[0].style.backgroundColor = "red";
     })
 });
